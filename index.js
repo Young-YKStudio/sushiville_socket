9cdc51767b5b6f7b1958e5bf515dde2b639faecb
Young-YKStudio <young@ykstudio.dev>
2022-06-06 17:09:17 -0400

    moved cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const socketio = require('socket.io');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const socketUtils = require('./utils/socketUtils');
+app.use(cors());
 
 const app = express();
 const server = http.createServer(app);
@@ -13,7 +14,6 @@ const io = socketio(server, {'pingTimeout': 7000, 'pingInterval': 3000, cors: {o
 connectDB();
 socketUtils.connection(io);
 
-app.use(cors());
 app.use('/public', express.static('public'));
 app.get('/', (req, res) => {
   res.json({
