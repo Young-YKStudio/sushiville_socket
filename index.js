a4ba5f67fabe60049209298c0d1b3c700effc02f
Young-YKStudio <young@ykstudio.dev>
2022-06-06 17:33:22 -0400

    updated...

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const { createServer } = require('http');
-const socketio = require('socket.io');
+const { server } = require('socket.io');
 const cors = require('cors');
+
+const app = express();
+const httpServer = createServer(app);
 const connectDB = require('./config/db');
 const socketUtils = require('./utils/socketUtils');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server, {'pingTimeout': 7000, 'pingInterval': 3000, cors: {origin: '*', methods: ["GET", "POST"]}});
+app.use(cors());
+
+const io = new Server(httpServer, {'pingTimeout': 7000, 'pingInterval': 3000, cors: {origin: '*', methods: ["GET", "POST"]}});
 
 connectDB();
 socketUtils.connection(io);
